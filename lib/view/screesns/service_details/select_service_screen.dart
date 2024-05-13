3bb9639be87277cfe8be3eb42b330f3da1dd4bae
nahiduzzaman11 <nahiduzzaman15-12736@diu.edu.bd>
2024-05-13 17:46:24 +0600

    select_service develop

--- lib/view/screesns/service_details/select_service_screen.dart
+++ lib/view/screesns/service_details/select_service_screen.dart
@@ -1,16 +1,115 @@
 import 'package:barbar_app/utils/colors/app_colors.dart';
+import 'package:barbar_app/utils/images/app_images.dart';
+import 'package:barbar_app/view/widgets/app_bar/custom_app_bar.dart';
+import 'package:barbar_app/view/widgets/button/custom_button.dart';
+import 'package:barbar_app/view/widgets/text/custom_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class SelectServiceScreen extends StatelessWidget {
+class SelectServiceScreen extends StatefulWidget {
   const SelectServiceScreen({super.key});
 
   @override
+  State<SelectServiceScreen> createState() => _SelectServiceScreenState();
+}
+
+class _SelectServiceScreenState extends State<SelectServiceScreen> {
+
+  bool clicked = false;
+
+  @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context);
+    var width = MediaQuery.of(context).size.width;
     return SafeArea(
       top: true,
       child: Scaffold(
         extendBody: true,
         backgroundColor: AppColors.white_100,
+        appBar: CustomAppBar(
+          spreadRadius: 0.r,
+          appBarContent: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: SizedBox(
+                  height: 42.h,
+                  width: 42.w,
+                  child: Icon(Icons.arrow_back_ios,
+                      size: 20.h, color: AppColors.white_10),
+                ),
+              ),
+              CustomText(
+                  text: "Choose Styles",
+                  fontSize: 18.sp,
+                  color: AppColors.purple_100),
+              SizedBox(height: 42.h, width: 42.w)
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 24.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: width.w,
+                height: 200.h,
+                margin: EdgeInsets.only(bottom: 24.h),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.r),
+                  image: const DecorationImage(image: AssetImage(AppImages.banner), fit: BoxFit.fill),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+                    margin: EdgeInsets.only(bottom: 8.h),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: AppColors.purple_60, width: 1.w),
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomText(text: "Hair Cut", bottom: 8.h),
+                            CustomText(text: "\$30.00", fontSize: 12.sp),
+                          ],
+                        ),
+                        Switch(
+                            value: clicked,
+                            activeColor: AppColors.purple_100,
+                            inactiveThumbColor: AppColors.purple_40,
+                            trackColor: const MaterialStatePropertyAll(AppColors.purple_5),
+                            onChanged: (bool value){
+                              setState(() {
+                                clicked = !clicked;
+                              });
+                            },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              SizedBox(height: 24.h),
+              CustomElevatedButton(onPressed: (){}, titleText: "Continue", buttonWidth: width)
+            ],
+          ),
+        ),
       ),
     );
   }
