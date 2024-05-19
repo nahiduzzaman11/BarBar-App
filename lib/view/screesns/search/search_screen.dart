eb5ef683cc41dbfc869416d3e81af37bdd72c5f2
nahiduzzaman11 <nahiduzzaman15-12736@diu.edu.bd>
2024-05-19 12:28:25 +0600

    search_screen develop

--- lib/view/screesns/search/search_screen.dart
+++ lib/view/screesns/search/search_screen.dart
@@ -1,4 +1,14 @@
+import 'package:barbar_app/utils/colors/app_colors.dart';
+import 'package:barbar_app/utils/icons/app_icons.dart';
+import 'package:barbar_app/view/widgets/app_bar/custom_app_bar.dart';
+import 'package:barbar_app/view/widgets/image/custom_image.dart';
+import 'package:barbar_app/view/widgets/tab_bar/custom_tab_bar.dart';
+import 'package:barbar_app/view/widgets/text/custom_text.dart';
+import 'package:barbar_app/view/widgets/textfield/custom_textfield.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,8 +18,71 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  int _selectedIndex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Scaffold());
+    ScreenUtil.init(context);
+
+    return SafeArea(
+      top: true,
+      child: Scaffold(
+        extendBody: true,
+        backgroundColor: AppColors.white_100,
+        appBar: CustomAppBar(
+          spreadRadius: 0.r,
+          appBarHeight: 84.h,
+          appBarContent: CustomTextField(
+            textInputAction: TextInputAction.done,
+            hintText: "Search salons/barbers/products",
+            hintStyle: GoogleFonts.lato(fontSize: 14.sp, color: AppColors.white_60),
+            prefixIcon: const CustomImage(imageSrc: AppIcons.search, imageColor: AppColors.white_60),
+          ),
+        ),
+        body: SingleChildScrollView(
+          physics: const ClampingScrollPhysics(),
+          padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 24.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomTab(
+                titles: const ['Salon', 'Barbers', 'Products'],
+                onTabSelected: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                selectedIndex: _selectedIndex,
+              ),
+
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 16.h),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.access_time_sharp, size: 20.h, color: AppColors.white_20),
+                            CustomText(text: "City Salon", fontSize: 18.sp, left: 16.w)
+                          ],
+                        ),
+                        Icon(Icons.cancel_outlined, color: AppColors.white_10, size: 20.h)
+                      ],
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/widgets/tab_bar/custom_tab_bar.dart
@@ -0,0 +1,69 @@
+import 'package:barbar_app/utils/colors/app_colors.dart';
+import 'package:barbar_app/view/widgets/text/custom_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomTab extends StatelessWidget {
+  final List<String> titles;
+  final Function(int) onTabSelected;
+  final int selectedIndex;
+
+  const CustomTab(
+      {super.key,
+      required this.titles,
+      required this.onTabSelected,
+      required this.selectedIndex
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: width,
+      padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 8.h),
+      margin: EdgeInsets.only(bottom: 16.h),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(color: AppColors.purple_60, width: 1.w),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: List.generate(
+          titles.length,
+          (index) => _buildTab(
+            titles[index],
+            index + 1,
+            width,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTab(String title, int tabIndex, double width) {
+    final bool isSelected = selectedIndex == tabIndex;
+
+    return Flexible(
+      child: GestureDetector(
+        onTap: () {
+          onTabSelected(tabIndex);
+        },
+        child: Container(
+          width: width,
+          padding: EdgeInsets.symmetric(vertical: 8.h),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.r),
+            color: isSelected ? AppColors.purple_100 : Colors.white,
+          ),
+          child: CustomText(
+            text: title,
+            color: isSelected ? AppColors.white_100 : AppColors.white_10,
+            fontSize: 14.sp,
+          ),
+        ),
+      ),
+    );
+  }
+}
